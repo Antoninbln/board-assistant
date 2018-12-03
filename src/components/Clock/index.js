1726655fe379e71685f1b5fcd2101129b231aa24
AntoninBln <antonin.blin.pro@gmail.com>
2018-12-03 12:40:05 +0100

    Style: Add clock style

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -19,10 +19,16 @@ class Clock extends Component {
 
   render() {
     const { clock } = this.state;
+    const s = <span className="separator"> : </span>;
 
     return (
       <div className={`c-clock ${styles.clock} sk-skeleton`}>
-        <span className={`${!clock ? "sk-text-short sk-light" : ""}`}>{clock || "&nbps;"}</span>
+        {clock && (
+          <React.Fragment>
+            <span className={`${!clock ? "sk-text-short sk-light" : ""} c-clock__day`}>{clock.day || "&nbps;"}</span>
+            <span className={`${!clock ? "sk-text-short sk-light" : ""} c-clock__time`}>{clock.hours}{s}{clock.minutes}{s}{clock.seconds}</span>
+          </React.Fragment>
+        )}
       </div>
     );
   }
--- src/components/utils/dates.js
+++ src/components/utils/dates.js
@@ -4,7 +4,13 @@ import localization from "moment/locale/fr"; // eslint-disable-line
 moment.locale("fr", localization);
 
 export default function getCurrentDate() {
-  return moment().format("DD MMM YYYY HH:mm:ss");
+  return {
+    "date": moment().format("DD MMM YYYY HH:mm:ss"),
+    "day": moment().format("DD MMM YYYY"),
+    "hours": moment().format("HH"),
+    "minutes": moment().format("mm"),
+    "seconds": moment().format("ss")
+  };
 }
 
 export function getCurrentDay(details = false) {
