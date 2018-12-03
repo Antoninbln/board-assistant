2db6aebdcdcc7588e3090f7c57257a98537a988a
AntoninBln <antonin.blin.pro@gmail.com>
2018-12-03 12:45:12 +0100

    Docs: Add dates documentation

--- src/components/utils/dates.js
+++ src/components/utils/dates.js
@@ -3,6 +3,10 @@ import localization from "moment/locale/fr"; // eslint-disable-line
 
 moment.locale("fr", localization);
 
+/**
+ * Return current date object
+ * @returns { Object<date: String, day: String, hours: String, minutes: String, seconds: String>}
+ */
 export default function getCurrentDate() {
   return {
     "date": moment().format("DD MMM YYYY HH:mm:ss"),
@@ -11,9 +15,14 @@ export default function getCurrentDate() {
     "minutes": moment().format("mm"),
     "seconds": moment().format("ss")
   };
-}
+};
 
+/**
+ * Return current day
+ * @param { Boolean } details 
+ * @return { String }
+ */
 export function getCurrentDay(details = false) {
   if (details) return moment().format("DD MMM YYYY");
   return moment().format("DD");
-}
+};
