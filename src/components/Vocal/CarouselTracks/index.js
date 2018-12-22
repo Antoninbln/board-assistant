f17e427a1ba67f152c689f68cb1af9f397eafbf1
Thanh-Thao Hoang <thanhthaohng@gmail.com>
2018-12-22 22:27:06 +0100

    Style: display gradient div for no cover

--- src/components/Vocal/CarouselTracks/index.js
+++ src/components/Vocal/CarouselTracks/index.js
@@ -5,9 +5,21 @@ import styles from "./index.module.scss";
 
 const CarouselTracks = ({ cover, previousTrack, nextTrack }) => (
   <div className='spotify__carousel'>
-    {previousTrack && <img className="spotify__carousel__besideTrack previousTrack" src={getCover(previousTrack)} alt="Cover previous track" /> }
+    {previousTrack
+      ? (
+        <img className="spotify__carousel__besideTrack previousTrack" src={getCover(previousTrack)} alt="Cover previous track" />
+      ) : (
+        <div className="spotify__carousel__besideTrack noCover previousTrack"></div>
+      )
+    }
     {cover ? <img className="spotify__carousel__currentTrack" src={cover} alt="Pochette d'album" /> : <p>No cover available</p>}
-    {nextTrack && <img className="spotify__carousel__besideTrack nextTrack" src={getCover(nextTrack)} alt="Cover next track" /> }
+    {nextTrack
+      ? (
+        <img className="spotify__carousel__besideTrack nextTrack" src={getCover(nextTrack)} alt="Cover next track" />
+      ) : (
+        <div className="spotify__carousel__besideTrack noCover nextTrack"></div>
+      )
+    }
   </div>
 );
 
